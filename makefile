9f23afda08420c5c510f98d08f8506388d3e30ad
MicchelFAB <micchelfab@gmail.com>
2023-09-12 18:38:19 +0100

    Update

---
+++ makefile
@@ -0,0 +1,97 @@
+# ------- FONT AND COLORS ------- #
+GREY		:= \e[1;30m
+RED			:= \e[1;31m
+GREEN		:= \e[1;32m
+YELLOW		:= \e[1;33m
+BLUE		:= \e[1;34m
+MAGENTA		:= \e[1;35m
+CYAN		:= \e[1;36m
+WHITE		:= \e[1;37m
+
+BOLD		:= \e[1m
+DIM			:= \e[2m	
+UNDERLINE	:= \e[4m
+BLINK		:= \e[5m
+INVERTED	:= \e[7m
+END			:= \e[0m
+
+# ------- COMMANDS ------- #
+
+CC			:= cc
+RM			:= rm -rf
+
+# ------- FLAGS ------- #
+CFLAGS		:= -Wall -Wextra -Werror -g
+CPPFLAGS	:= -MMD -MP 
+RDLINE		:= -lreadline -lXext -lX11 -lm
+MAKEFLAGS	+= --no-print-directory
+MKDIR_P		:= mkdir -p
+
+# ------- PATHS ------- #
+INCLUDE		=	-I ./inc/\
+				-I ./libft/
+
+# ------- PROJECT ------- #
+NAME		:= minishell
+
+# ------- SOURCES ------- #
+SRC_DIR		:= src/
+SRC_LIB		:= src/libft
+SRCS		:= main.c
+
+SRCS		:= $(SRCS:%=$(SRC_DIR)/%)
+
+BUILD_DIR	:= .build
+LIBFT		:= $(BUILD_DIR)/libs/libft.a
+OBJS		:= $(SRCS:$(SRC_DIR)/%.c=$(BUILD_DIR)/obj/%.o)
+DEPS		:= $(OBJS:.o=.d)
+
+# ------- RULES ------- #
+# ------- ALL ------- #
+all: $(NAME)
+
+# ------- $(NAME) ------- #
+$(NAME): $(OBJS)
+	@make -C $(SRC_LIB)
+	@cp $(SRC_LIB)/libft.a $(LIBFT)
+	@printf "\e[2K\r$(GREEN)Generated $(NAME)$(END)\n"
+	@$(CC) $(CFLAGS) $(CPPFLAGS) $(OBJS) $(LIBFT) $(RDLINE) -o $(NAME)
+
+# ------- OBJECTS ------- #
+$(BUILD_DIR)/obj/%.o: $(SRC_DIR)/%.c | $(BUILD_DIR)
+	@printf "\e[2K\r$(YELLOW)Compiling $<$(END)"
+	@$(CC) $(CFLAGS) $(CPPFLAGS) $(RDLINE) -c -O3 $< -o $@ $(INCLUDE)
+
+# ------- DEPS ------- #
+-include $(DEPS)
+
+# ------- BUILD_DIR ------- #
+$(BUILD_DIR):
+	@$(MKDIR_P) $(BUILD_DIR)/libs $(BUILD_DIR)/obj 
+
+# ------- CLEAN ------- #
+clean:
+	@printf "\e[2K\r$(RED)Cleaning objects from $(NAME)$(END)\n"
+	@make -C $(SRC_LIB) clean
+	@$(RM) $(OBJS) $(DEPS) $(BUILD_DIR)
+
+# ------- FCLEAN ------- #
+fclean: clean
+	@printf "\e[2K\r$(RED)Cleaning executables from $(NAME)$(END)\n"
+	@make -C $(SRC_LIB) fclean
+	@$(RM) $(NAME)
+
+# ------- RE ------- #
+re:
+	@$(MAKE) fclean
+	@$(MAKE) all
+
+# ------- TEST LEAKS ------- #
+leak: all
+	@valgrind -q --leak-check=full --show-leak-kinds=all --suppressions=readline_supression ./$(NAME)
+
+# ------- RECORD LOG ------- #
+log: all
+	@valgrind --leak-check=full --show-leak-kinds=all --suppressions=readline_supression --log-file=log.txt ./$(NAME)
+
+.PHONY: clean fclean re all bonus leak log
--- src/main.c
+++ src/main.c
@@ -6,28 +6,40 @@
 /*   By: mamaral- <mamaral-@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 11:40:49 by mamaral-          #+#    #+#             */
-/*   Updated: 2023/09/12 09:45:46 by mamaral-         ###   ########.fr       */
+/*   Updated: 2023/09/12 18:21:34 by mamaral-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// ft_check_signal() is a function that checks if the signal is SIGINT or SIGQUIT.
+/**
+ * @param void is what the function receives.
+ * @param sigaction is a structure that contains the signal handler.
+ * @brief 
+ * ft_ignore_signal() is a function that ignores the signal.
+ * ignore.sa_handler = SIG_IGN; is a signal handler that ignores the signal.
+ * sigemptyset(&ignore.sa_mask); is a function that initializes the signal set.
+ * ingore.sa_flags = 0; is a flag that specifies the set of signals to be blocked, is set to 0 so that no signals are blocked.
+ * sigaction(SIGINT, &ignore, NULL); is a function that changes the action taken by a process on receipt from CTRL+C.
+ * sigaction(SIGTSTP, &ignore, NULL); is a function that changes the action taken by a process on receipt from CTRL+Z.
+ * sigaction(SIGQUIT, &ignore, NULL); is a function that changes the action taken by a process on receipt from CTRL+\.
+ * @return 
+*/
+void	ft_ignore_signal(void)
+{
+	struct sigaction	ignore;
 
-// int	ft_check_signal(void)
-// {
-// 	struct sigaction	sa;
-// 	int		signal = 0;
-
-// 	sa.sa_handler = SIG_IGN;
-// 	sigemptyset(&sa.sa_mask);
-// 	sa.sa_flags = 0;
-// 	if (sigaction(SIGINT, &sa, NULL) == 1)
-// 		signal = SIGINT;
-// 	if (sigaction(SIGQUIT, &sa, NULL) == 1)
-// 		signal = SIGQUIT;
-// 	return (signal);
-// }
+	ignore.sa_handler = SIG_IGN;
+	sigemptyset(&ignore.sa_mask);
+	ignore.sa_flags = 0;
+	
+	sigaction(SIGINT, &ignore, NULL);
+	sigaction(SIGTSTP, &ignore, NULL);
+	sigaction(SIGQUIT, &ignore, NULL);
+	sigaction(11, &ignore, NULL);
+	
+}
+	
 
 // init_shell() is a function that allocates memory for the shell structure and
 // initializes the environment variable.
@@ -53,23 +65,17 @@ void	loop_shell(t_shell *shell)
 	while (true)
 	{
 		line = readline("minishell -> "); 
-		//line = get_next_line(shell->fd);
-		if (ft_strcmp(line, "exit") == 0)
+		if (!line || !ft_strcmp(line, "exit"))
 		{
 			free(line);
+			free(shell);
+			ft_printf("exit\n");
 			exit(0);
 		}
 		ft_printf("%s\n", line);
 		free(line);
-		// shell->signal = ft_check_signal();
-		// if (shell->signal == SIGINT)
-		// 	exit(0);
 	}
 }
-
-// ft_debug_terminal() is a function that prints the terminal name.
-
-
 
 int	main(int ac, char **av, char **env) // ac = argument count, av = argument vector, env = environment
 {
@@ -77,6 +83,7 @@ int	main(int ac, char **av, char **env) // ac = argument count, av = argument ve
 
 	(void)ac;
 	(void)av;
+	ft_ignore_signal();
 	shell = init_shell(env);
 	loop_shell(shell);
 	return (0);
