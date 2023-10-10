627dd8d3ab538b55cb313885eb6240302264f6c1
BelmiroRodrigues <barodrigues@outlook.pt>
2023-10-10 14:59:45 +0100

    bilt

--- parse/execute.c
+++ parse/execute.c
@@ -1,9 +1,13 @@
 #include "../inc/minishell.h"
 
+/* void	exec_cmd(char	**cmd, ) */
+
 void	execute(char **cmd, int *fd, int *std_in, t_shell *shell)
 {
 	(void)fd;
 	(void)std_in;
 	if (is_builtin(cmd))
 		exec_builtin(&cmd[0], shell);
+/* 	else
+		exec_cmd(&cmd[0], fd, std_in, shell); */
 }
